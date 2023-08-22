ca69abdd2e4d6b90f8dca07278d9c6d0d01fba7c
jkosata <j.kosata@protonmail.com>
2023-08-22 19:23:16 +0200

    Parameters.jl testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using AbstractFields, Test
+using AbstractFields, Test, Parameters
 
 @with_fields abs_type custom_float::Float64 custom_int::Int
 
@@ -27,3 +27,14 @@ end
 @test_throws "syntax: duplicate" @macroexpand @abs_type_weak struct invalid_type; custom_int end
 
 @test_throws "syntax: duplicate" @macroexpand @with_fields invalid_type custom_field custom_field
+
+# tests below pass with an appropriate modification of Parameters.jl to unwrap macrocalls
+@with_kw @abs_type struct kw_struct
+    new_field
+    new_field_def=1
+end
+
+@test all( (in(f, fieldnames(kw_struct)) for f in [:new_field, :new_field_def, :custom_int, :custom_float]))
+
+kw_inst = kw_struct(custom_float=1, custom_int=2, new_field=[])
+@test kw_inst.new_field_def == 1
